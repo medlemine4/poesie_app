08a4003a229bc86cc7d2f1fbeff7165bf473b106
ahmedsalem <22050@supnum.mr>
2024-05-25 02:56:37 +0000

    fffffffffg

--- poesie_app/lib/screens/poet_page.dart
+++ poesie_app/lib/screens/poet_page.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'package:poesie_app/screens/full_screen_image_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:poesie_app/screens/DeewanParAuteurPage.dart';
 import 'package:poesie_app/screens/PoetDetails.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'full_screen_image_page.dart';
+import 'package:poesie_app/screens/DeewanParAuteurPage.dart';
 import 'package:poesie_app/screens/SearchPage.dart';
 import '../data/mongo_database.dart';
 
@@ -16,6 +18,7 @@ class _PoetPageState extends State<PoetPage> {
   String _searchText = '';
   List<String> favoriteAuthors = [];
   List<Map<String, dynamic>> poetsList = [];
+  String? _sortBy = 'name';
 
   @override
   void initState() {
@@ -54,11 +57,26 @@ class _PoetPageState extends State<PoetPage> {
     List<Map<String, dynamic>> data = await MongoDataBase.getPoetDetailsList();
     setState(() {
       poetsList = data;
+      _sortPoets();
     });
+  }
+
+  void _sortPoets() {
+    if (_sortBy == 'name') {
+      poetsList.sort((a, b) => a['nom'].compareTo(b['nom']));
+    } else if (_sortBy == 'date_naissance') {
+      poetsList.sort((a, b) =>
+          (a['date_naissance'] as int).compareTo(b['date_naissance'] as int));
+    } else {
+      // Reset the filter and reload the poet details
+      loadPoetDetails();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    _sortPoets();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -93,58 +111,180 @@ class _PoetPageState extends State<PoetPage> {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.3),
-                    spreadRadius: 3,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: TextField(
-                controller: _searchController,
-                decoration: InputDecoration(
-                  hintText: '...ابحث عن الشاعر عن طريق القائمة',
-                  hintStyle: TextStyle(fontFamily: 'Almarai', fontSize: 14.0),
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30.0),
-                    borderSide: BorderSide.none,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 3,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
                   ),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      _searchController.clear();
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: '...ابحث عن الشاعر عن طريق القائمة',
+                      hintStyle:
+                          TextStyle(fontFamily: 'Almarai', fontSize: 14.0),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          _searchController.clear();
+                          setState(() {
+                            _searchText = '';
+                          });
+                        },
+                        icon: Icon(Icons.clear, color: Colors.grey),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 20.0),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide:
+                            BorderSide(color: Colors.grey.shade300, width: 1.5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide:
+                            BorderSide(color: Colors.grey.shade600, width: 1.5),
+                      ),
+                    ),
+                    onChanged: (value) {
                       setState(() {
-                        _searchText = '';
+                        _searchText = value;
                       });
                     },
-                    icon: Icon(Icons.clear, color: Colors.grey),
-                  ),
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide:
-                        BorderSide(color: Colors.grey.shade300, width: 1.5),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide:
-                        BorderSide(color: Colors.grey.shade600, width: 1.5),
                   ),
                 ),
-                onChanged: (value) {
-                  setState(() {
-                    _searchText = value;
-                  });
-                },
-              ),
+                SizedBox(height: 15),
+                Container(
+                  width: 12, // Adjust the width of the DropdownButton
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 3,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.teal[600]!,
+                            Colors.teal[300]!,
+                            Colors.teal[300]!
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.teal.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 4,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton<String>(
+                          value: _sortBy,
+                          icon:
+                              Icon(Icons.arrow_drop_down, color: Colors.white),
+                          iconSize: 24,
+                          elevation: 16,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              if (newValue == "إلغاء التصفية") {
+                                _sortBy = null; // Reset the filter
+                              } else {
+                                _sortBy = newValue!;
+                              }
+                            });
+                          },
+                          items: <String>[
+                            'إلغاء التصفية',
+                            'name',
+                            'date_naissance'
+                          ] // Add Cancel Filter option
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: MouseRegion(
+                                cursor: SystemMouseCursors.click,
+                                onEnter: (event) => setState(() {}),
+                                onExit: (event) => setState(() {}),
+                                child: Tooltip(
+                                  message: value == 'name'
+                                      ? 'Sort by Name'
+                                      : (value == 'date_naissance'
+                                          ? 'Sort by Date of Birth'
+                                          : 'إلغاء التصفية'),
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 8, vertical: 4),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      color: Colors.white,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black12,
+                                          blurRadius: 4,
+                                          offset: Offset(2, 2),
+                                        ),
+                                      ],
+                                    ),
+                                    child: Text(
+                                      value == 'name'
+                                          ? 'الاسم'
+                                          : (value == 'date_naissance'
+                                              ? 'تاريخ الميلاد'
+                                              : 'إلغاء التصفية'),
+                                      style: TextStyle(
+                                        fontSize: 19,
+                                        color: Colors.teal[700],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
           Expanded(
@@ -163,7 +303,8 @@ class _PoetPageState extends State<PoetPage> {
                       bool isFavorite = favoriteAuthors.contains(authorId);
 
                       return Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
                         child: Container(
                           decoration: BoxDecoration(
                             color: Colors.teal[100],
@@ -183,9 +324,9 @@ class _PoetPageState extends State<PoetPage> {
                                 context,
                                 MaterialPageRoute(
                                   builder: (context) => DeewanParAuteurPage(
-                                    authorId: authorId,
                                     poetFirstname: nom,
                                     poetLastname: prenom,
+                                    authorId: authorId,
                                   ),
                                 ),
                               );
@@ -206,7 +347,8 @@ class _PoetPageState extends State<PoetPage> {
                               child: Hero(
                                 tag: 'hero-$nom',
                                 child: CircleAvatar(
-                                  backgroundImage: AssetImage('images/$nom.jpg'),
+                                  backgroundImage:
+                                      AssetImage('images/$nom.jpg'),
                                   radius: 30,
                                 ),
                               ),
@@ -214,8 +356,8 @@ class _PoetPageState extends State<PoetPage> {
                             title: Text(
                               nom,
                               style: TextStyle(
-                                fontFamily: 'Almarai',
                                 fontSize: 20.0,
+                                fontFamily: 'Almarai',
                                 fontWeight: FontWeight.bold,
                                 color: Colors.teal[900],
                               ),
@@ -226,25 +368,28 @@ class _PoetPageState extends State<PoetPage> {
                                 Text(
                                   prenom,
                                   style: TextStyle(
+                                    fontSize: 18.0,
                                     fontFamily: 'Almarai',
                                     fontWeight: FontWeight.bold,
-                                    fontSize: 19.0,
                                     color: Colors.teal[900],
                                   ),
                                 ),
+                                SizedBox(height: 5.0),
                                 Text(
-                                  'عدد الدواوين: $deewanCount',
+                                  'الدواوين: $deewanCount',
                                   style: TextStyle(
-                                    fontFamily: 'Amiri',
                                     fontSize: 16.0,
+                                    fontFamily: 'Almarai',
+                                    fontWeight: FontWeight.w400,
                                     color: Colors.teal[700],
                                   ),
                                 ),
                                 Text(
-                                  'عدد القصائد: $poemCount',
+                                  'القصائد: $poemCount',
                                   style: TextStyle(
-                                    fontFamily: 'Amiri',
                                     fontSize: 16.0,
+                                    fontFamily: 'Almarai',
+                                    fontWeight: FontWeight.w400,
                                     color: Colors.teal[700],
                                   ),
                                 ),
@@ -258,7 +403,8 @@ class _PoetPageState extends State<PoetPage> {
                                     Navigator.push(
                                       context,
                                       MaterialPageRoute(
-                                        builder: (context) => PoetDetails(poetName: nom),
+                                        builder: (context) =>
+                                            PoetDetails(poetName: nom),
                                       ),
                                     );
                                   },
@@ -267,18 +413,19 @@ class _PoetPageState extends State<PoetPage> {
                                     color: Colors.teal[900],
                                   ),
                                 ),
-                                SizedBox(width: 10),
                                 IconButton(
+                                  icon: Icon(
+                                    isFavorite
+                                        ? Icons.favorite
+                                        : Icons.favorite_border,
+                                    color:
+                                        isFavorite ? Colors.red : Colors.grey,
+                                  ),
                                   onPressed: () {
                                     setState(() {
-                                      isFavorite = !isFavorite;
+                                      toggleFavorite(authorId);
                                     });
-                                    toggleFavorite(authorId);
                                   },
-                                  icon: Icon(
-                                    isFavorite ? Icons.favorite : Icons.favorite_border,
-                                    color: isFavorite ? Colors.red : Colors.teal[900],
-                                  ),
                                 ),
                               ],
                             ),
@@ -294,16 +441,19 @@ class _PoetPageState extends State<PoetPage> {
   }
 
   List<Map<String, dynamic>> _getFilteredPoets() {
+    List<Map<String, dynamic>> filteredPoets;
     if (_searchText.isEmpty) {
-      return poetsList;
+      filteredPoets = poetsList;
     } else {
-      return poetsList
+      filteredPoets = poetsList
           .where((poet) => _searchInPoet(poet, _searchText.toLowerCase()))
           .toList();
     }
+    return filteredPoets;
   }
 
   bool _searchInPoet(Map<String, dynamic> poet, String searchText) {
-    return poet.values.any((value) => value.toString().toLowerCase().contains(searchText));
+    return poet['nom'].toLowerCase().contains(searchText) ||
+        poet['prenom'].toLowerCase().contains(searchText);
   }
 }
