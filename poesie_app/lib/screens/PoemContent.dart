cc8440daeed6ebb130b1a27cf63d73bfbb5a24b9
ahmedsalem <22050@supnum.mr>
2024-06-05 14:10:52 +0000

    asewrtdghjku

--- poesie_app/lib/screens/PoemContent.dart
+++ poesie_app/lib/screens/PoemContent.dart
@@ -1,26 +1,89 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:flutter/services.dart';
 
-class PoemContent extends StatelessWidget {
+class PoemContent extends StatefulWidget {
   final String poemTitle;
   final String poemContent;
 
   PoemContent({required this.poemTitle, required this.poemContent});
 
   @override
+  _PoemContentState createState() => _PoemContentState();
+}
+
+class _PoemContentState extends State<PoemContent> {
+  double _fontSize = 18.0;
+  bool _isFavorite = false;
+  late ThemeData _themeData;
+
+  @override
+  void initState() {
+    super.initState();
+    _themeData = _buildLightTheme();
+  }
+
+  ThemeData _buildLightTheme() {
+    return ThemeData(
+      brightness: Brightness.light,
+      primaryColor: Colors.teal[700],
+      colorScheme: ColorScheme.light(
+        primary: Colors.teal[700]!,
+        secondary: Colors.teal[200]!,
+      ),
+      textTheme: TextTheme(
+        bodyLarge: TextStyle(fontSize: _fontSize, fontFamily: 'Almarai'),
+      ),
+    );
+  }
+
+  ThemeData _buildDarkTheme() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.teal[900],
+      colorScheme: ColorScheme.dark(
+        primary: Colors.teal[900]!,
+        secondary: Colors.teal[600]!,
+      ),
+      textTheme: TextTheme(
+        bodyLarge: TextStyle(fontSize: _fontSize, fontFamily: 'Almarai'),
+      ),
+    );
+  }
+
+  void _sharePoem() {
+    final String contentToShare =
+        "${widget.poemTitle.isEmpty ? 'Untitled Poem' : widget.poemTitle}\n\n${widget.poemContent}";
+    Share.share(contentToShare);
+  }
+
+  void _copyPoem() {
+    final String contentToCopy =
+        "${widget.poemTitle.isEmpty ? 'Untitled Poem' : widget.poemTitle}\n\n${widget.poemContent}";
+    Clipboard.setData(ClipboardData(text: contentToCopy));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Poem copied to clipboard')),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Ensure poem content is not empty
-    if (poemContent.isEmpty) {
+    if (widget.poemContent.isEmpty) {
       return Scaffold(
         appBar: AppBar(
-          title: Text(
-            'Poem',
-            style: TextStyle(
+          title: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              'Poem',
+              style: TextStyle(
                 fontSize: 27.0,
                 fontFamily: 'Almarai',
                 fontWeight: FontWeight.bold,
-                color: Colors.white),
+                color: Colors.white,
+              ),
+            ),
           ),
           centerTitle: true,
           backgroundColor: Colors.teal[700],
@@ -28,83 +91,154 @@ class PoemContent extends StatelessWidget {
         body: Center(
           child: Text(
             'No content available',
-            style: TextStyle(fontSize: 18.0, fontFamily: 'Almarai'),
+            style: TextStyle(
+              fontSize: 18.0,
+              fontFamily: 'Almarai',
+            ),
           ),
         ),
       );
     }
 
-    List<String> lines = poemContent
+    List<String> lines = widget.poemContent
         .split('\n')
         .where((line) => line.trim().isNotEmpty)
         .toList();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          poemTitle.isEmpty ? 'Untitled Poem' : poemTitle,
-          style: const TextStyle(
-              fontSize: 27.0,
-              fontFamily: 'Almarai',
-              fontWeight: FontWeight.bold,
-              color: Colors.white),
+    return Theme(
+      data: _themeData,
+      child: Scaffold(
+        appBar: AppBar(
+          title: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              widget.poemTitle.isEmpty ? 'Untitled Poem' : widget.poemTitle,
+              style: TextStyle(
+                fontSize: 27.0,
+                fontFamily: 'Almarai',
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+          centerTitle: true,
+          backgroundColor: _themeData.primaryColor,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.brightness_6),
+              onPressed: () {
+                setState(() {
+                  _themeData = _themeData.brightness == Brightness.dark
+                      ? _buildLightTheme()
+                      : _buildDarkTheme();
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.copy),
+              onPressed: _copyPoem,
+            ),
+          ],
         ),
-        iconTheme: const IconThemeData(
-          color: Colors.white,
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.teal[700],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.teal[50]!, Colors.teal[100]!],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                _themeData.colorScheme.secondary.withOpacity(0.1),
+                _themeData.colorScheme.secondary.withOpacity(0.3)
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  for (int i = 0; i < lines.length; i += 1)
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 300),
+                      margin: EdgeInsets.symmetric(vertical: 6.0),
+                      padding: EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        color: (i ~/ 2) % 2 == 0
+                            ? _themeData.colorScheme.secondary.withOpacity(0.5)
+                            : _themeData.colorScheme.secondary.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(8.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: i % 2 == 0
+                            ? MainAxisAlignment.start
+                            : MainAxisAlignment.end,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              lines[i],
+                              style: TextStyle(
+                                fontFamily: 'Almarai',
+                                fontSize: _fontSize,
+                                height:
+                                    1.5, // Improved line height for better readability
+                                letterSpacing:
+                                    0.5, // Slight letter spacing for clarity
+                                color: _themeData.primaryColorDark,
+                              ),
+                              textDirection: i % 2 == 0
+                                  ? TextDirection.rtl
+                                  : TextDirection.ltr,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  SizedBox(height: 20), // Extra spacing at the bottom
+                ],
+              ),
+            ),
           ),
         ),
-        child: SingleChildScrollView(
+        floatingActionButton: FloatingActionButton(
+          onPressed: _sharePoem,
+          backgroundColor: _themeData.primaryColor,
+          child: Icon(Icons.share),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 6.0,
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                for (int i = 0; i < lines.length; i += 1)
-                  Container(
-                    margin: const EdgeInsets.symmetric(vertical: 6.0),
-                    padding: const EdgeInsets.all(12.0),
-                    decoration: BoxDecoration(
-                      color: (i ~/ 2) % 2 == 0 ? Colors.white : Colors.teal[50],
-                      borderRadius: BorderRadius.circular(8.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 1,
-                          blurRadius: 5,
-                          offset: Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: Row(
-                      mainAxisAlignment: i % 2 == 0
-                          ? MainAxisAlignment.start
-                          : MainAxisAlignment.end,
-                      children: [
-                        Expanded(
-                          child: Text(
-                            lines[i],
-                            style: const TextStyle(
-                              fontFamily: 'Almarai',
-                              fontSize: 18.0,
-                            ),
-                            textDirection: i % 2 == 0
-                                ? TextDirection.rtl
-                                : TextDirection.ltr,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                IconButton(
+                  icon: Icon(Icons.zoom_out),
+                  onPressed: () {
+                    setState(() {
+                      if (_fontSize > 12.0) _fontSize -= 2.0;
+                    });
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.zoom_in),
+                  onPressed: () {
+                    setState(() {
+                      if (_fontSize < 36.0) _fontSize += 2.0;
+                    });
+                  },
+                ),
               ],
             ),
           ),
--- poesie_app/lib/screens/PoetDetails.dart
+++ poesie_app/lib/screens/PoetDetails.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:poesie_app/screens/SearchPage.dart';
 import '../data/mongo_database.dart';
+import 'dart:io';
 
 class PoetDetails extends StatelessWidget {
   final String poetName;
@@ -10,113 +13,213 @@ class PoetDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(
-            'تفاصيل الشاعر',
-            style: TextStyle(
-                fontSize: 27.0,
-                fontFamily: 'Almarai',
-                fontWeight: FontWeight.bold,
-                color: Colors.white),
-          ),
-          iconTheme: IconThemeData(
-            color: Colors.white,
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.teal[700],
-          actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SearchPage(),
-                  ),
-                );
-              },
-              icon: Icon(Icons.search),
-              color: Colors.white,
-            ),
-          ]),
-      body: SingleChildScrollView(
-        child: FutureBuilder<Map<String, dynamic>>(
-          future: MongoDataBase.getPoetDetails(poetName),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else {
-              Map<String, dynamic>? poetDetails = snapshot.data;
-              if (poetDetails == null || poetDetails.isEmpty) {
-                return Center(child: Text('Poet not found'));
-              }
-
-              return Column(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 400.0,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'تفاصيل الشاعر',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontFamily: 'Almarai',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              background: Stack(
+                fit: StackFit.expand,
                 children: [
-                  // Afficher l'image de l'auteur
+                  Hero(
+                    tag: 'poetImage_$poetName',
+                    child: _buildPoetImage(context, poetName),
+                  ),
                   Container(
-                    width: double.infinity,
-                    height: 200,
                     decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                            'images/$poetName.jpg'), // Remplacer par le chemin correct
-                        fit: BoxFit.cover,
+                      gradient: LinearGradient(
+                        colors: [Colors.black54, Colors.transparent],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.center,
                       ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      'الوصف',
-                      style: TextStyle(
-                        fontFamily: 'Amiri',
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      'عدد الدواوين: ${poetDetails['deewanCount']}',
-                      style: TextStyle(
-                        fontFamily: 'Amiri',
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      'عدد القصائد: ${poetDetails['poemCount']}',
-                      style: TextStyle(
-                        fontFamily: 'Amiri',
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text(
-                      poetDetails['description'] ?? 'N/A',
-                      style: TextStyle(fontFamily: 'Amiri', fontSize: 18.0),
-                      textDirection: TextDirection
-                          .rtl, // Aligner le texte de droite à gauche
                     ),
                   ),
                 ],
-              );
-            }
-          },
+              ),
+              centerTitle: true,
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SearchPage(),
+                    ),
+                  );
+                },
+                icon: Icon(Icons.search),
+                color: Colors.white,
+              ),
+            ],
+            backgroundColor: Colors.teal[700],
+          ),
+          SliverToBoxAdapter(
+            child: FutureBuilder<Map<String, dynamic>>(
+              future: MongoDataBase.getPoetDetails(poetName),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else {
+                  Map<String, dynamic>? poetDetails = snapshot.data;
+                  if (poetDetails == null || poetDetails.isEmpty) {
+                    return Center(child: Text('Poet not found'));
+                  }
+
+                  return Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _buildSectionTitle('الوصف', Colors.teal[800]),
+                        SizedBox(height: 20),
+                        _buildDetailRow(
+                          icon: Icons.library_books,
+                          title: 'عدد الدواوين',
+                          content: '${poetDetails['deewanCount']}',
+                        ),
+                        SizedBox(height: 10),
+                        _buildDetailRow(
+                          icon: Icons.article,
+                          title: 'عدد القصائد',
+                          content: '${poetDetails['poemCount']}',
+                        ),
+                        SizedBox(height: 20),
+                        _buildDescriptionCard(
+                          poetDetails['description'] ?? 'N/A',
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPoetImage(BuildContext context, String poetName) {
+    String imagePath = 'images/$poetName.jpg';
+    bool imageExists = File(imagePath).existsSync();
+
+    return imageExists
+        ? Image.asset(
+            imagePath,
+            fit: BoxFit.cover,
+          )
+        : Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30.0),
+                bottomRight: Radius.circular(30.0),
+              ),
+            ),
+            child: Center(
+              child: Icon(
+                Icons.person,
+                color: Colors.white,
+                size: 100.0,
+              ),
+            ),
+          );
+  }
+
+  Widget _buildSectionTitle(String title, Color? color) {
+    return Center(
+      child: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'Amiri',
+          fontSize: 26.0,
+          fontWeight: FontWeight.bold,
+          color: color,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget _buildDetailRow({
+    required IconData icon,
+    required String title,
+    required String content,
+  }) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Text(
+          content,
+          style: TextStyle(
+            fontFamily: 'Amiri',
+            fontSize: 20.0,
+            color: Colors.grey[800],
+          ),
+        ),
+        SizedBox(width: 10),
+        Text(
+          title,
+          style: TextStyle(
+            fontFamily: 'Amiri',
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.teal[800],
+          ),
+        ),
+        SizedBox(width: 10),
+        Icon(icon, color: Colors.teal[700]),
+      ],
+    );
+  }
+
+  Widget _buildDescriptionCard(String description) {
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'وصف الشاعر',
+              style: TextStyle(
+                fontFamily: 'Amiri',
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal[800],
+              ),
+              textDirection: TextDirection.rtl,
+            ),
+            SizedBox(height: 10),
+            Text(
+              description,
+              style: TextStyle(
+                fontFamily: 'Amiri',
+                fontSize: 18.0,
+                height: 1.5,
+                color: Colors.grey[800],
+              ),
+              textDirection: TextDirection.rtl,
+            ),
+          ],
         ),
       ),
     );
